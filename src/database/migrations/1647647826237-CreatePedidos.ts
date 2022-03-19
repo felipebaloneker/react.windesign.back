5edc0c2dc3aa48a1ad8eb9badb96f95e5fba85a8
hunter <hunter>
2022-03-18 21:27:11 -0300

    modified

--- src/database/migrations/1647647826237-CreatePedidos.ts
+++ src/database/migrations/1647647826237-CreatePedidos.ts
@@ -1,11 +1,55 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreatePedidos1647647826237 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:'pedidos',
+                columns:[
+                    {
+                        name:'id',
+                        type:'uuid',
+                        isPrimary:true
+                    },
+                    {
+                        name:'author_id',
+                        type:'uuid'
+                    },
+                    {
+                        name:'detalhes',
+                        type:'varchar'
+                    },
+                    {
+                        name:'setor',
+                        type:'varchar'
+                    },
+                    {
+                        name:'status',
+                        type:'varchar'
+                    },
+                    {
+                        name:'created_at',
+                        type:'timestamp',
+                        default:'now()'
+                    }
+                ],
+                foreignKeys:[
+                    {
+                        name:"FKAuthorIdCompliments",
+                        referencedTableName:"users",
+                        referencedColumnNames:["id"],
+                        columnNames:["author_id"],
+                        onDelete:"SET NULL",
+                        onUpdate:"SET NULL"
+                    },
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("pedidos");
     }
 
 }
