a74791c5c96a350723da1107ae4dbbea3cf854a9
hunter <hunter>
2022-03-18 21:59:35 -0300

    criado service para users

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,32 @@
+import {hash} from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repository/UserRepository';
+
+interface IUserRequest{
+    name:string;
+    email:string;
+    password:string;
+}
+
+class CreateUserService{
+    async execute({name, email, password}:IUserRequest){
+        const usersRepository = getCustomRepository(UserRepository);
+        if(!email){
+            throw new Error('Email incorrect');
+        }
+        const userAlreadyExists = await usersRepository.findOne({email})
+        if(userAlreadyExists){
+            throw new Error('User alreary exists')
+        }
+        const passwordHash = await hash(password, 8)
+
+        const user = usersRepository.create({
+            name,
+            email,
+            password:passwordHash
+        })
+        await usersRepository.save(user)
+        return user;
+    }
+}
+export {CreateUserService}
