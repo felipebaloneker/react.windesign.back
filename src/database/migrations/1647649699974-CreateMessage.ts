e29df764fd71844b70eb9994dc747c5d40f63fb3
hunter <hunter>
2022-03-18 21:33:34 -0300

    criada tabela mensagens

---
+++ src/database/migrations/1647649699974-CreateMessage.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateMessage1647649699974 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:'message',
+                columns:[
+                    {
+                        name:'id',
+                        type:'uuid',
+                        isPrimary:true
+                    },
+                    {
+                        name:'author_id',
+                        type:'uuid',
+                    },
+                    {
+                        name:'pedido_id',
+                        type:'uuid',
+                    },
+                    {
+                        name:'body',
+                        type:'varchar'
+                    },
+                    {
+                        name:'created_at',
+                        type:'timestamp',
+                        default:'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
