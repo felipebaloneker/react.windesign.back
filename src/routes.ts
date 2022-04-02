6f53342dbeaeb7b1650051eacca5e1afa40971f3
felipe <felipebaloneker.com>
2022-04-02 15:24:26 -0300

    created login user router

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,10 @@ import { CreateUserController } from './controllers/CreateUserController';
 const routes = Router();
 
 const createUserController  = new CreateUserController();
+const authenticateUserController = new CreateTagController();
+
 
 routes.post('/users/create', createUserController.handle);
+routes.post('/users/login', authenticateUserController.handle);
 
 export {routes}
