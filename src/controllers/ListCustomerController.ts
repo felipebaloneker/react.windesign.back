4d1cb013fa17a15c30ab7508fc7c84c768eb51c4
felipe <felipebaloneker.com>
2022-04-03 00:12:40 -0300

    created controller to list customers

---
+++ src/controllers/ListCustomerController.ts
@@ -0,0 +1,13 @@
+import {Request, Response} from 'express',
+import {ListCustomerService} from '../services/ListCustomerService'
+
+class ListCustomerController{
+    async handle(request:Request, response:Response){
+        const {user_id} = request;
+        const listCustomerService = new ListCustomerService();
+
+        const customers = await listCustomerService.execute()
+
+        return response.json(customers);
+    }
+}
