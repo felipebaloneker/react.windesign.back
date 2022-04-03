2da7e19bd3d926773821343a88056be171772880
felipe <felipebaloneker.com>
2022-04-03 00:37:19 -0300

    created route to list employee

--- src/routes.ts
+++ src/routes.ts
@@ -4,14 +4,17 @@ import { CreateUserController } from './controllers/CreateUserController';
 import {ListCustomerController} from './controllers/ListCustomerController'
 import { verifyAuthenticated } from './middlewares/verifyAuthenticated';
 import { ensureManager } from './middlewares/ensureManager';
+import { ListEmployeeController } from './controllers/ListEmployeeController';
 const routes = Router();
 
 const createUserController  = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const listCustomerController = new ListCustomerController();
+const listEmployeeController = new ListEmployeeController();
 
 routes.post('/users/create', createUserController.handle);
 routes.post('/users/login', authenticateUserController.handle);
 
-routes.get('/users/customers', verifyAuthenticated,ensureManager,listCustomerController.handle);
+routes.get('/users/list/customer', verifyAuthenticated,ensureManager,listCustomerController.handle);
+routes.get('/users/list/employee', verifyAuthenticated,ensureManager,listEmployeeController.handle);
 export {routes}
