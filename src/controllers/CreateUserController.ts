e8f27e9c4dcd6ddbed8d61de79b83bf4931717aa
felipe <felipebaloneker.com>
2022-04-02 23:36:50 -0300

    changed usertype for type

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,10 +3,10 @@ import {CreateUserService} from '../services/CreateUserService'
 
 class CreateUserController{
     async handle(request:Request, response:Response){
-        const {name,email,password,usertype} = request.body;
+        const {name,email,password,type} = request.body;
 
         const createUserService = new CreateUserService();
-        const user = await createUserService.execute({name,email,password,usertype})
+        const user = await createUserService.execute({name,email,password,type})
 
         return response.json(user)
     }
