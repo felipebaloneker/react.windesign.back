5b1b536b2a568fdfbb87c43f8daec0ae59cb80b8
felipe <felipebaloneker.com>
2022-04-03 00:19:45 -0300

    created router to list users

--- src/controllers/ListCustomerController.ts
+++ src/controllers/ListCustomerController.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express',
+import {Request, Response} from 'express'
 import {ListCustomerService} from '../services/ListCustomerService'
 
 class ListCustomerController{
@@ -11,3 +11,4 @@ class ListCustomerController{
         return response.json(customers);
     }
 }
+export { ListCustomerController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import {Router} from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateUserController } from './controllers/CreateUserController';
+import {ListCustomerController} from './controllers/ListCustomerController'
+import { verifyAuthenticated } from './middlewares/verifyAuthenticated';
 
 const routes = Router();
 
 const createUserController  = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
-
+const listCustomerController = new ListCustomerController();
 
 routes.post('/users/create', createUserController.handle);
 routes.post('/users/login', authenticateUserController.handle);
 
+routes.get('/users/customers', verifyAuthenticated,listCustomerController.handle);
 export {routes}
