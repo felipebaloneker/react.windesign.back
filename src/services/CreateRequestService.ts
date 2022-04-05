09ad6abaeee520d4c639c29937d87a37a5d0cd21
felipe <felipebaloneker.com>
2022-04-05 20:26:28 -0300

    updated

---
+++ src/services/CreateRequestService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from "typeorm";
+import { RequestRepository } from "../repository/RequestRepository";
+
+interface IRequestType{
+    author_id: string;
+    details: string;
+    category_id:string;
+    status:string;
+    created_at:string;
+}
+
+class CreateRequestService{
+    async execute({author_id, details, category_id, status}:IRequestType){
+        const requestRepository = getCustomRepository(RequestRepository);
+        if(!author_id && !details){
+            throw new Error('Empty Field')
+        }
+        const request = requestRepository.create({
+            author_id,
+            details,
+            category_id,
+            status
+        })
+        await requestRepository.save(request)
+    }
+}
+export {CreateRequestService}
